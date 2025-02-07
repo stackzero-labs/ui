58b3b2f98ab039be413dbe4e4cce6a52d8d471ef
marcello-stackzero <marcello.novelli@stackzero.co>
2025-02-07 16:04:13 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 import localFont from "next/font/local";
 import type { ReactNode } from "react";
 import "./global.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,8 +14,21 @@ const geistSans = localFont({
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const isDev = process.env.NODE_ENV === "development";
+
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      data-website-id="b4c4225c-b9a3-4199-bd56-d9fc8d987e1a"
+    >
+      {!isDev ? (
+        <Script
+          async
+          src="https://cloud.umami.is/script.js"
+          data-website-id="b4c4225c-b9a3-4199-bd56-d9fc8d987e1a"
+        />
+      ) : null}
       <body
         className={cn(
           GeistSans.variable,
