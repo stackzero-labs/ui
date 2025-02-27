1c910e987e82bafc27467c9718a7fa0f289038a7
marcello-stackzero <marcello.novelli@stackzero.co>
2025-02-27 10:48:40 +0100

    fix/og-image-generation

--- app/docs-og/[...slug]/route.tsx
+++ app/docs-og/[...slug]/route.tsx
@@ -1,11 +1,14 @@
-import { generateOGImage } from "fumadocs-ui/og";
+// import { generateOGImage } from "fumadocs-ui/og";
+import { generateOGImage } from "@/lib/generateOGImage";
 import { metadataImage } from "@/lib/metadata";
 
-export const GET = metadataImage.createAPI((page) => {
+export const GET = metadataImage.createAPI(async (page) => {
   return generateOGImage({
     description: page.data.description,
     site: "stackzero/commerce-ui",
     title: page.data.title,
+    primaryColor: "#8940ff34",
+    primaryTextColor: "rgb(240, 228, 247)",
   });
 });
 
